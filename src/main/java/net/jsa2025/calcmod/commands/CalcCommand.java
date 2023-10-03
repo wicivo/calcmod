77dc54d87a8d099c22d87ba9baae0037fe5b472b
wicivo <116191734+wicivo@users.noreply.github.com>
2023-10-03 21:28:44 +0500

    Update CalcCommand.java

--- src/main/java/net/jsa2025/calcmod/commands/CalcCommand.java
+++ src/main/java/net/jsa2025/calcmod/commands/CalcCommand.java
@@ -221,7 +221,7 @@ public class CalcCommand {
         }
         messageText.append(Text.literal(" "));
 
-        source.sendMessage(messageText.append(Text.literal("§7[Click to Copy]§f").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, m.replaceAll("§a", "").replaceAll("§f", ""))))));
+        source.sendMessage(messageText.append(Text.literal("§7[Нажмите чтобы скопировать]§f").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, m.replaceAll("§a", "").replaceAll("§f", ""))))));
     }
     
     public static void sendMessageServer(ServerCommandSource source, CalcMessageBuilder messageBuilder) {
